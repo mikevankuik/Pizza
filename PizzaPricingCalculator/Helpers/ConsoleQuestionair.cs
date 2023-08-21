f785cf6d92f652240fd38fc6caa9e6d90137b6f3
Mike van Kuik <mikevankuik@gmail.com>
2023-08-21 19:56:23 +0200

    Moved logic from Program.cs to ConsoleQuestions.cs Cleaned up Program.cs

---
+++ PizzaPricingCalculator/Helpers/ConsoleQuestionair.cs
@@ -0,0 +1,38 @@
+﻿using PizzaLibrary.Helpers;
+using PizzaLibrary.Models;
+
+namespace PizzaPricingCalculator.Helpers;
+
+public static class ConsoleQuestionair
+{
+    public static void PizzaDetails(bool morePizza, string metric, string currency)
+    {
+        List<PizzaModel> pizzaList = new();
+        // Ask for to enter the size of the pizza and the price
+        // Ask if there is another pizza until the user says no
+        // Then calculate for each pizza the price per sqare cm / inch 
+        int i = 0;
+        while (morePizza == true)
+        {
+            i++;
+            string? pizzaSize = Questionnaire.Question($"How big is the pizza? (diameter in {metric})", "1");
+            pizzaSize = Sanitation.ReplaceDotWithComma(pizzaSize);
+            _ = double.TryParse(pizzaSize, out double sizeResult);
+
+            string? pizzaPrice = Questionnaire.Question("What does it cost?", "1");
+            pizzaPrice = Sanitation.ReplaceDotWithComma(pizzaPrice);
+            _ = decimal.TryParse(pizzaPrice, out decimal priceResult);
+
+            double pizzaSurface = Calculatons.Surface(sizeResult);
+            decimal surfaceResult = (decimal)pizzaSurface;
+
+            pizzaList.Add(new PizzaModel() { Number = i, Diameter = sizeResult, Price = priceResult, Surface = surfaceResult, PPSA = Calculatons.PricePerSquareArea(priceResult, surfaceResult) });
+            string morePizzaQuestion = Questionnaire.Question("Would you like to add another pizza to compare? (Y/N)", "Y");
+            morePizza = morePizzaQuestion == "N" || morePizzaQuestion == "n" ? false : true;
+        }
+        foreach (var pizza in pizzaList)
+        {
+            Console.WriteLine($"{pizza.Number}: {pizza.Diameter} {metric} | {currency} {pizza.Price} | Pizza Surface Area {pizza.Surface} Square {metric} | Price Per Surface Area: {currency} {pizza.PPSA}");
+        }
+    }
+}
--- PizzaPricingCalculator/Program.cs
+++ PizzaPricingCalculator/Program.cs
@@ -1,10 +1,10 @@
-﻿using PizzaLibrary.Models;
-using PizzaLibrary.Helpers;
+﻿using PizzaLibrary.Helpers;
+using PizzaPricingCalculator.Helpers;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 bool morePizza = true;
-List<PizzaModel> pizzaList = new();
+//List<PizzaModel> pizzaList = new();
 
 // Welcome the user
 Console.WriteLine("Welcome to the Pizza Price Calculator");
@@ -25,31 +25,4 @@ switch (currencyType)
     default: currency = "€"; break;
 }
 Console.WriteLine($"{ currency } will be used");
-
-// Ask for to enter the size of the pizza and the price
-// Ask if there is another pizza until the user says no
-// Then calculate for each pizza the price per sqare cm / inch 
-int i = 0;
-while (morePizza == true)
-{
-    i++;
-    string? pizzaSize = Questionnaire.Question($"How big is the pizza? (diameter in { metric })", "1");
-    pizzaSize = Sanitation.ReplaceDotWithComma(pizzaSize);
-    _ = double.TryParse(pizzaSize, out double sizeResult);
-
-    string? pizzaPrice = Questionnaire.Question("What does it cost?","1");
-    pizzaPrice = Sanitation.ReplaceDotWithComma(pizzaPrice);
-    _ = decimal.TryParse(pizzaPrice, out decimal priceResult);
-
-    double pizzaSurface = Calculatons.Surface(sizeResult);
-    decimal surfaceResult = (decimal)pizzaSurface;
-
-    pizzaList.Add(new PizzaModel() { Number = i, Diameter = sizeResult, Price = priceResult, Surface = surfaceResult, PPSA = Calculatons.PricePerSquareArea(priceResult, surfaceResult) });
-    string morePizzaQuestion = Questionnaire.Question("Would you like to add another pizza to compare? (Y/N)", "Y");
-    morePizza = morePizzaQuestion == "N" || morePizzaQuestion == "n" ? false : true;
-}
-
-foreach (var pizza in pizzaList)
-{
-    Console.WriteLine($"{ pizza.Number }: { pizza.Diameter } { metric } | {currency} { pizza.Price } | Pizza Surface Area { pizza.Surface } Square { metric } | Price Per Surface Area: { currency } { pizza.PPSA }");
-}
+ConsoleQuestionair.PizzaDetails(morePizza, metric, currency);
