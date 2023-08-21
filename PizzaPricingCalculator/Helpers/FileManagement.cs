c209fb441f5131448570d20a479e58cdd96b4af1
Mike van Kuik <mikevankuik@gmail.com>
2023-08-21 20:39:41 +0200

    Removed the last logic from Program.cs Added FileManagement.cs Created logic to generate a config.ini file which will be used to store the configuration.

---
+++ PizzaPricingCalculator/Helpers/FileManagement.cs
@@ -0,0 +1,47 @@
+﻿using PizzaLibrary.Helpers;
+using System.Security;
+
+namespace PizzaPricingCalculator.Helpers
+{
+    public static class FileManagement
+    {
+        public static string[] GetConfiguration(string? defaultPath)
+        {
+            if (string.IsNullOrEmpty(defaultPath))
+            {
+                defaultPath = @"config.ini";
+            }
+
+            if (File.Exists(defaultPath)) 
+            {
+                var result = File.ReadAllLines(defaultPath);
+                return result;
+            }
+            else 
+            {
+                string? metericType = Questionnaire.Question("Will you be using the metic or the imperial system? (M for Meteric I for Imperial)", "M");
+                string metric = metericType == "I" ? "Inch" : "CM";
+                Console.WriteLine($"{metric} will be used");
+
+                // Set Euro, Dollar or Pound
+                string? currencyType = Questionnaire.Question("What currency do you use ? (E for Euro, D for Dollar P for Pound)", "E");
+
+                string currency = string.Empty;
+                switch (currencyType)
+                {
+                    case "E": currency = "€"; break;
+                    case "D": currency = "$"; break;
+                    case "P": currency = "£"; break;
+                    default: currency = "€"; break;
+                }
+                Console.WriteLine($"{currency} will be used");
+
+                string[] config = { metric, currency };
+                File.WriteAllLines(defaultPath, config );
+
+                string[] result = { metric, currency };
+                return result ; 
+            }
+        }
+    }
+}
--- PizzaPricingCalculator/Program.cs
+++ PizzaPricingCalculator/Program.cs
@@ -1,28 +1,16 @@
-﻿using PizzaLibrary.Helpers;
-using PizzaPricingCalculator.Helpers;
+﻿using PizzaPricingCalculator.Helpers;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 bool morePizza = true;
-//List<PizzaModel> pizzaList = new();
 
 // Welcome the user
 Console.WriteLine("Welcome to the Pizza Price Calculator");
-// Set Inches or CM
-string? metericType = Questionnaire.Question("Will you be using the metic or the imperial system? (M for Meteric I for Imperial)","M");
-string metric = metericType == "I" ? "Inch" : "CM";
-Console.WriteLine($"{ metric } will be used");
 
-// Set Euro, Dollar or Pound
-string? currencyType = Questionnaire.Question("What currency do you use ? (E for Euro, D for Dollar P for Pound)", "E");
+// Get configuration
+var config = FileManagement.GetConfiguration("");
+string metric = config.GetValue(0).ToString();
+string currency = config.GetValue(1).ToString();
 
-string currency = string.Empty;
-switch (currencyType)
-{
-    case "E": currency = "€"; break;
-    case "D": currency = "$"; break;
-    case "P": currency = "£"; break;
-    default: currency = "€"; break;
-}
-Console.WriteLine($"{ currency } will be used");
+// Print out results
 ConsoleQuestionair.PizzaDetails(morePizza, metric, currency);
